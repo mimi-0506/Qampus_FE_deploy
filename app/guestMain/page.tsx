073bee40cd4f59b689eec31331a3fbdfde2acdb0
mimi <62527898+mimi-0506@users.noreply.github.com>
2025-02-12 20:11:12 +0900

    feat: Page3, 5, 7 추가 (미완성)

--- app/guestMain/page.tsx
+++ app/guestMain/page.tsx
@@ -1,7 +1,12 @@
+import Footer from './_components/Footer';
 import Page1 from './_components/Page1';
 import Page2 from './_components/Page2';
+import Page3 from './_components/Page3';
 import Page4 from './_components/Page4';
+import Page5 from './_components/Page5';
 import Page6 from './_components/Page6';
+import Page7 from './_components/Page7';
+
 export const metadata = {
   title: '게스트 메인 | MyApp',
   description: '로그인하지 않은 사용자를 위한 메인 페이지입니다.',
@@ -12,8 +17,12 @@ export default function GuestMainPage() {
     <main className="scrollbar-hide flex flex-col items-center justify-center min-h-screen overflow-y-auto ">
       <Page1 />
       <Page2 />
+      <Page3 />
       <Page4 />
+      <Page5 />
       <Page6 />
+      <Page7 />
+      <Footer />
     </main>
   );
 }
