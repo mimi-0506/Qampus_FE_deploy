92ef7b20e3d83c9221fde2cfb0d7390410870631
mimi <62527898+mimi-0506@users.noreply.github.com>
2025-02-12 18:14:08 +0900

    feat: Page2 완성

--- app/guestMain/page.tsx
+++ app/guestMain/page.tsx
@@ -1,4 +1,5 @@
 import Page1 from './_components/Page1';
+import Page2 from './_components/Page2';
 export const metadata = {
   title: '게스트 메인 | MyApp',
   description: '로그인하지 않은 사용자를 위한 메인 페이지입니다.',
@@ -8,6 +9,7 @@ export default function GuestMainPage() {
   return (
     <main className="scrollbar-hide flex flex-col items-center justify-center min-h-screen overflow-y-auto ">
       <Page1 />
+      <Page2 />
     </main>
   );
 }
