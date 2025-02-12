06441ab2659d3eec01504a1ac1f61f18258f408c
mimi <62527898+mimi-0506@users.noreply.github.com>
2025-02-12 19:55:12 +0900

    feat: Page6 완성

--- app/guestMain/page.tsx
+++ app/guestMain/page.tsx
@@ -1,5 +1,6 @@
 import Page1 from './_components/Page1';
 import Page2 from './_components/Page2';
+import Page6 from './_components/Page6';
 export const metadata = {
   title: '게스트 메인 | MyApp',
   description: '로그인하지 않은 사용자를 위한 메인 페이지입니다.',
@@ -10,6 +11,7 @@ export default function GuestMainPage() {
     <main className="scrollbar-hide flex flex-col items-center justify-center min-h-screen overflow-y-auto ">
       <Page1 />
       <Page2 />
+      <Page6 />
     </main>
   );
 }
