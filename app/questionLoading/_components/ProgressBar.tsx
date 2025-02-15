32ad5487846be5b5e419f4f7cc48aa7a4ecf6f36
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-02-16 02:22:03 +0900

    feat: 질문 등록 로딩중 페이지 추가

---
+++ app/questionLoading/_components/ProgressBar.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import {useState, useEffect} from 'react';
+import {useRouter} from 'next/navigation';
+
+export default function ProgressBar() {
+  const [progress, setProgress] = useState(0);
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress(prev => {
+        if (prev >= 100) {
+          clearInterval(timer);
+          setTimeout(() => {
+            router.push('/questionDone');
+          });
+          return 100;
+        }
+        return prev + 1;
+      });
+    }, 30);
+
+    return () => clearInterval(timer);
+  }, [router]);
+
+  return (
+    <div className="w-[400px] h-2.5 bg-[#F4F4F4] mt-4 mb-20 rounded-full overflow-hidden">
+      <div
+        className="h-full bg-[#7BA1FF] rounded-full transition-all duration-30 ease-linear"
+        style={{width: `${progress}%`}}
+      ></div>
+    </div>
+  );
+}
---
+++ app/questionLoading/page.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import Image from 'next/image';
+import ProgressBar from './_components/ProgressBar';
+
+export default function QuestionLoading() {
+  return (
+    <main
+      className="h-screen w-screen bg-white flex flex-col items-center justify-center bg-center overflow-y-hidden"
+      style={{
+        backgroundImage: `url('/images/question/question_loading_bg.png')`,
+        backgroundSize: '68%',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <Image
+        src="/images/characters/grin.png"
+        alt="grin icon"
+        width={300}
+        height={300}
+      />
+      <p className="text-[#3765D6] text-3xl font-[600] mt-10">
+        질문을 등록 중입니다
+      </p>
+      <p className="text-[#606060] text-lg mt-2">잠시만 기다려주세요...</p>
+      <ProgressBar />
+    </main>
+  );
+}
