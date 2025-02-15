c7df1246e040d102e94ff9f68051155f2b928f6c
mimi <0506phm@gmail.com>
2025-02-16 01:42:00 +0900

    refactor: page4 반응형 처리

--- app/guestMain/_components/Page4.tsx
+++ app/guestMain/_components/Page4.tsx
@@ -1,23 +1,25 @@
 import Image from 'next/image';
+
 export default function Page4() {
   return (
-    <div className="w-scree relative flex h-screen items-center justify-center rounded-tl-[80px] rounded-tr-[80px] bg-black text-white">
-      <div className="absolute left-[120px] top-[170px]">
-        <div className="from-maindark to-mainlight bg-gradient-to-r bg-clip-text text-transparent">
+    <div className="relative flex aspect-[16/9] w-screen items-center justify-center rounded-tl-[4.17vw] rounded-tr-[4.17vw] bg-black text-white">
+      <div className="absolute left-[6.25vw] top-[8.85vw]">
+        <div className="from-dark1 to-light1 bg-gradient-to-r bg-clip-text text-[0.9vw] text-transparent">
           Answer collection
         </div>
-        <h2 className="text-[28px">내가 받은 답변</h2>
+        <h2 className="text-[1.46vw]">내가 받은 답변</h2>
       </div>
 
       <div className="flex w-screen items-center justify-around">
-        <Image
-          src="/images/main/bubbles.png"
-          width={275}
-          height={204}
-          alt="bubble"
-        />
-
-        <div className="text-[36px]">
+        <div className="w-[14vw] aspect-[275/204]">
+          <Image
+            src="/images/main/bubbles.png"
+            width={275}
+            height={204}
+            alt="bubble"
+          />
+        </div>
+        <div className="text-[1.87vw]">
           Qampus에서는 자신이 질문한 내용에 달린 답변들을 모아서 확인할 수
           있어요
           <br />
