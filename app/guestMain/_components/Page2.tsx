830568a249c81ada309470896df1b03009342996
mimi <0506phm@gmail.com>
2025-02-15 20:55:55 +0900

    refactor: page2 반응형 처리

--- app/guestMain/_components/Page2.tsx
+++ app/guestMain/_components/Page2.tsx
@@ -1,27 +1,29 @@
 import Image from 'next/image';
 export default function Page2() {
   return (
-    <div className="flex h-screen w-full items-center justify-between">
-      <div className="ml-[118px] flex h-[238] flex-col items-start justify-between">
-        <div className="from-maindark to-mainlight bg-gradient-to-r bg-clip-text text-[18px] text-transparent">
+    <div className="flex aspect-[16/9] w-screen items-center justify-between">
+      <div className="ml-[118px] flex aspect-[764/160] w-[40vw] flex-col items-start justify-between">
+        <div className="from-dark1 to-light1 bg-gradient-to-r bg-clip-text text-[0.9vw] text-transparent">
           About Qampus
         </div>
-        <div className="text-[64px] font-semibold">
-          다양한 사람들과 지식을 나누고 <br />
-          경쟁하며 함께 성장하는 서비스
+        <div className="text-[3.3vw] font-bold">
+          다양한 사람들과 지식을 나누고 경쟁하며 함께 성장하는 서비스
         </div>
-        <div className="text-mainpage2gray text-[20px]">
+        <div className="text-grey2 text-[1vw]">
           자유로운 질문과 답변으로 서로의 지식을 공유하고 학교 별 선의의 경쟁을
           통해 지식 성장을 도모해요
         </div>
       </div>
-      <Image
-        className="mr-[392px]"
-        src="/images/characters/smile.png"
-        width={357}
-        height={360}
-        alt=""
-      />
+
+      <div className="relative w-[18.5vw] aspect-[357/360] mr-[20vw]">
+        <Image
+          src="/images/characters/smile.png"
+          width={357}
+          height={360}
+          layout="responsive"
+          alt=""
+        />
+      </div>
     </div>
   );
 }
