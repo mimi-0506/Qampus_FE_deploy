46a89168af7afb6efee6d023ec5b8ac741d3b74c
mimi <0506phm@gmail.com>
2025-02-16 01:40:06 +0900

    feat: page6 완성

--- app/guestMain/_components/Page6.tsx
+++ app/guestMain/_components/Page6.tsx
@@ -1,86 +1,72 @@
 import Image from 'next/image';
 
 export default function Page6() {
-  const DATA = [
-    {
-      medal: 'gold',
-      field: '환경',
-      question:
-        '지속 가능한 환경 기술들을 개발할 때 경제적 타당성과 환경적 이점을 동시에 충족시키는 방법은 뭔가요?',
-      info: '조회수 982회 · 나도 궁금해요 215개',
-    },
-    {
-      medal: 'silver',
-      field: '경영',
-      question:
-        '여행사에서 판매하는 사회적 약자를 위한 관광상품(휠체어 이동 지원)도 esg 경영을 실천하는 방법일까요?',
-      info: '조회수 815회 · 나도 궁금해요 174개',
-    },
-    {
-      medal: 'bronze',
-      field: '디자인',
-      question:
-        'ai를 활용해서 만든 디자인의 저작권은 제작자에게 있다고 봐야 하나요?',
-      info: '조회수 798회 · 나도 궁금해요 155개',
-    },
-  ];
-
-  const Card = ({
-    medal,
-    field,
-    question,
-    info,
-  }: {
-    medal: string;
-    field: string;
-    question: string;
-    info: string;
-  }) => {
-    // medal 값에 따라 left 값을 동적으로 설정
-    const leftValue = medal === 'silver' ? '80px' : '-80px';
-
-    return (
-      <div
-        className="bg-mainblack relative box-border flex h-[208px] w-[1254px] items-center gap-[44px] rounded-3xl p-[15px]"
-        style={{left: leftValue}} // 동적으로 left 값 적용
-      >
-        <div className="relative h-[107px] w-[97px]">
-          <Image src={`/images/main/medal_${medal}.png`} fill alt="medal" />
-        </div>
-        <div className="flex flex-col gap-[18px]">
-          <div className="bg-maindrakgray text-maingray flex h-[41px] w-[107px] items-center justify-center rounded-[51px] text-[20px]">
-            {field}
-          </div>
-          <div className="text-[26px] text-white">{question}</div>
-          <div className="text-mainpage2gary text-[20px]">{info}</div>
-        </div>
-      </div>
-    );
-  };
-
   return (
-    <div className="to-maindarkblue flew flew-col relative h-screen w-screen items-center justify-center bg-gradient-to-b from-black">
+    <div className="aspect-[16/9] flex flex-col relative w-screen items-center justify-center bg-gradient-to-b from-black to-page6bg">
       <div className="flex flex-col items-center justify-center">
-        <div className="from-maindark to-mainlight bg-gradient-to-r bg-clip-text text-[18px] text-transparent">
+        <div className="from-dark2 to-light1 bg-gradient-to-r bg-clip-text text-[0.94vw] text-transparent">
           Popular Questions
         </div>
-        <div className="text-[28px] font-semibold text-white">
-          금주의 인기 질문
-        </div>
+        <div className="text-[1.46vw] text-white">금주의 인기 질문</div>
       </div>
 
-      <div className="mt-[160px] flex flex-col items-center gap-[36px]">
-        {DATA.map((data, index) => {
-          return (
-            <Card
-              key={index}
-              medal={data.medal}
-              field={data.field}
-              question={data.question}
-              info={data.info}
-            />
-          );
-        })}
+      <div className="mt-[8.33vw] flex flex-col w-screen gap-[1.87vw]">
+        {/* Card 1 */}
+        <div className="relative left-[12.3vw] box-border flex h-[10.83vw] w-[65.38vw] items-center gap-[2.29vw] p-[0.78vw] bg-[url('/images/main/box1_page6.png')] bg-cover bg-center">
+          <div className="relative h-[5.57vw] w-[5.06vw]">
+            <Image src="/images/main/medal_gold.png" fill alt="medal" />
+          </div>
+          <div className="flex flex-col gap-[0.94vw]">
+            <div className="bg-page6subtitle text-grey4 flex h-[2.14vw] w-[5.57vw] items-center justify-center rounded-[2.66vw] text-[1.04vw]">
+              환경
+            </div>
+            <div className="text-[1.35vw] text-white">
+              지속 가능한 환경 기술들을 개발할 때 경제적 타당성과 환경적 이점을
+              동시에 충족시키는 방법은 뭔가요?
+            </div>
+            <div className="text-grey2 text-[1.04vw]">
+              조회수 982회 · 나도 궁금해요 215개
+            </div>
+          </div>
+        </div>
+
+        {/* Card 2 */}
+        <div className="relative left-[22.5vw] box-border flex h-[10.83vw] w-[65.38vw] items-center gap-[2.29vw] p-[0.78vw] bg-[url('/images/main/box2_page6.png')] bg-cover bg-center">
+          <div className="relative h-[5.57vw] w-[5.06vw]">
+            <Image src="/images/main/medal_silver.png" fill alt="medal" />
+          </div>
+          <div className="flex flex-col gap-[0.94vw]">
+            <div className="bg-page6subtitle text-grey4 flex h-[2.14vw] w-[5.57vw] items-center justify-center rounded-[2.66vw] text-[1.04vw]">
+              경영
+            </div>
+            <div className="text-[1.35vw] text-white">
+              여행사에서 판매하는 사회적 약자를 위한 관광상품(휠체어 이동
+              지원)도 esg 경영을 실천하는 방법일까요?
+            </div>
+            <div className="text-grey2 text-[1.04vw]">
+              조회수 815회 · 나도 궁금해요 174개
+            </div>
+          </div>
+        </div>
+
+        {/* Card 3 */}
+        <div className="relative left-[15vw] box-border flex h-[10.83vw] w-[65.38vw] items-center gap-[2.29vw] p-[0.78vw] bg-[url('/images/main/box2_page6.png')] bg-cover bg-center">
+          <div className="relative h-[5.57vw] w-[5.06vw]">
+            <Image src="/images/main/medal_bronze.png" fill alt="medal" />
+          </div>
+          <div className="flex flex-col gap-[0.94vw]">
+            <div className="bg-page6subtitle text-grey4 flex h-[2.14vw] w-[5.57vw] items-center justify-center rounded-[2.66vw] text-[1.04vw]">
+              디자인
+            </div>
+            <div className="text-[1.35vw] text-white">
+              ai를 활용해서 만든 디자인의 저작권은 제작자에게 있다고 봐야
+              하나요?
+            </div>
+            <div className="text-grey2 text-[1.04vw]">
+              조회수 798회 · 나도 궁금해요 155개
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
