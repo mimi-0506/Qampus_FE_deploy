ea64ace6a3df8430ddf01715b16cbdb56c910177
mimi <0506phm@gmail.com>
2025-02-16 17:15:50 +0900

    feat: 인증 관련 세팅

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,21 @@
 'use client';
 
 import Image from 'next/image';
-import Link from 'next/link';
 
 export default function LoginPage() {
+  // const router = useRouter();
+
+  const loginWithKakao = () => {
+    // const url = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`;
+    // const tempUrl = '/login/info';
+    // router.push(tempUrl);
+  };
 
   return (
     <>
       <div className="mt-[5vw] gap-[0.7vw] flex flex-col text-[0.9vw]">
-        <Link
-          href={`https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&scope=profile_nickname,account_email,gender`}
+        <button
+          onClick={loginWithKakao}
           className="relative w-[20.4vw] aspect-[392/63] rounded-lg bg-yellow flex justify-center items-center box-border p-5"
         >
           <Image
@@ -20,7 +26,7 @@ export default function LoginPage() {
             className="absolute left-[1.1vw]"
           />
           카카오로 시작하기
-        </Link>
+        </button>
         <button className="relative w-[20.4vw] aspect-[392/63] rounded-lg bg-black text-white  flex justify-center items-center box-border p-5">
           <Image
             src="/svg/user.svg"
