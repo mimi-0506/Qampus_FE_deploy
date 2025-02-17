57423433b276dc6aafaadaf502c66940429a810a
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-02-18 00:29:16 +0900

    refactor: 프로그레스바 로직 최적화

--- app/questionLoading/_components/ProgressBar.tsx
+++ app/questionLoading/_components/ProgressBar.tsx
@@ -8,26 +8,25 @@ export default function ProgressBar() {
   const router = useRouter();
 
   useEffect(() => {
+    if (progress >= 100) {
+      const timeout = setTimeout(() => {
+        router.push('/questionDone');
+      }, 300);
+
+      return () => clearTimeout(timeout);
+    }
+
     const timer = setInterval(() => {
-      setProgress(prev => {
-        if (prev >= 100) {
-          clearInterval(timer);
-          setTimeout(() => {
-            router.push('/questionDone');
-          });
-          return 100;
-        }
-        return prev + 1;
-      });
+      setProgress(prev => Math.min(prev + 1, 100));
     }, 30);
 
     return () => clearInterval(timer);
-  }, [router]);
+  }, [progress, router]);
 
   return (
     <div className="w-[400px] h-2.5 bg-[#F4F4F4] mt-4 mb-20 rounded-full overflow-hidden">
       <div
-        className="h-full bg-[#7BA1FF] rounded-full transition-all duration-30 ease-linear"
+        className="h-full bg-[#7BA1FF] rounded-full transition-all duration-[30ms] ease-linear"
         style={{width: `${progress}%`}}
       />
     </div>
