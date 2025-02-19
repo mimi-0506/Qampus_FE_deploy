fcb42f6f2209f06c41e07e10ef50d9b73e4d6530
mimi <62527898+mimi-0506@users.noreply.github.com>
2025-02-19 20:30:45 +0900

    feat: 카카오 로그인 연동

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,15 @@
 'use client';
 
 import Image from 'next/image';
+import {useRouter} from 'next/navigation';
 
 export default function LoginPage() {
-  // const router = useRouter();
+  const router = useRouter();
 
   const loginWithKakao = () => {
-    // const url = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`;
-    // const tempUrl = '/login/info';
-    // router.push(tempUrl);
+    const url = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`;
+    const tempUrl = '/login/info';
+    router.push(url);
   };
 
   return (
