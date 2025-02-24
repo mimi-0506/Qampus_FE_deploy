9a748c22db5f3d52e1846837ebfeb59fb3fe658f
mimi <0506phm@gmail.com>
2025-02-24 20:39:17 +0900

    feat: 검은 화면 스크롤 애니메이션 추가

---
+++ app/guestMain/_components/Page5/LeftBox2.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import useScrollAnimation from '../useScrollAnimation';
+
+export default function LeftBox2() {
+  const {ref, isVisible} = useScrollAnimation();
+  return (
+    <>
+      <div
+        className={`box-border flex h-[11.5vw] w-[21.8vw] flex-col gap-3 p-[1.77vw]
+      opacity-0 ${isVisible ? 'animate-fadeIn' : ''}
+    bg-[url('/images/main/box2_page5.png')] bg-contain bg-no-repeat`}
+      >
+        <h2 className="text-[0.9vw] text-white">팀 대전</h2>
+        <p className="text-grey3 text-[0.78vw]">
+          챌린지 이벤트
+          <br />
+          특정 기간 동안 가장 많은 점수를 <br />
+          얻은 사용자에게 보상을 제공해요!
+        </p>
+      </div>
+      <div ref={ref} className="w-full  bg-white absolute bottom-0" />
+    </>
+  );
+}
--- app/guestMain/_components/Page7.tsx
+++ app/guestMain/_components/Page7.tsx
@@ -1,6 +1,9 @@
+'use client';
 import Image from 'next/image';
+import useScrollAnimation from './useScrollAnimation';
 
 export default function Page7() {
+  const {ref, isVisible} = useScrollAnimation();
   return (
     <div className="aspect-[16/9] w-screen overflow-hidden relative flex items-center justify-center">
       <Image
@@ -9,9 +12,16 @@ export default function Page7() {
         fill
         className="absolute top-0 z-0 "
       />
-      <div className="text-[3.3vw] font-bold text-white relative z-10">
+
+      <div
+        className={`text-[3.3vw] font-bold text-white relative z-10 opacity-0 ${
+          isVisible ? 'animate-fadeIn' : ''
+        }`}
+      >
         Qampus에서 성장하기
       </div>
+
+      <div ref={ref} className="w-full bg-white absolute bottom-0" />
     </div>
   );
 }
