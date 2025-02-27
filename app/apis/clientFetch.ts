87242756bef5549b30ad5ddf8561d558eed46ff9
mimi <0506phm@gmail.com>
2025-02-27 22:46:30 +0900

    feat: 클라이언트쪽 토큰 api추가

--- app/apis/clientFetch.ts
+++ app/apis/clientFetch.ts
@@ -1,3 +1,38 @@
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_DOMAIN;
+
+interface FetchOptions {
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE';
+  url: string;
+  body?: Record<string, unknown>;
+  cache?: boolean;
+}
+
+export async function fetchWithAuth({method, url, body, cache}: FetchOptions) {
+  const accessToken = document.cookie
+    .split('; ')
+    .find(row => row.startsWith('accessToken='))
+    ?.split('=')[1];
+
+  console.log('accessToken', accessToken);
+  if (!accessToken) window.location.href = '/logout';
+
+  const response = await fetch(`${API_BASE_URL}${url}`, {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: body ? JSON.stringify(body) : undefined,
+    cache: cache ? 'force-cache' : 'no-store',
+  });
+
+  const data = await response.json();
+
+  console.log('fetchWithAuth', data);
+
+  return data;
+}
+
 export async function fetchWithoutAuth({
   method = 'GET',
   url,
@@ -18,10 +53,7 @@ export async function fetchWithoutAuth({
     ...(method !== 'GET' && body ? {body: JSON.stringify(body)} : {}),
   };
 
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_DOMAIN}${url}`,
-    options,
-  );
+  const response = await fetch(`${API_BASE_URL}${url}`, options);
 
   const data = await response.json();
 
