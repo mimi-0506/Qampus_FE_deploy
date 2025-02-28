eb83d3df529d6744a2aae6082060578d5a0d405b
mimi <0506phm@gmail.com>
2025-02-28 22:29:59 +0900

    chore: 로그아웃 일부 주석처리

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -1,5 +1,5 @@
 import {createInfoStore} from '@/stores/search/infoStore';
-import {cookies} from 'next/headers';
+// import {cookies} from 'next/headers';
 import {redirect} from 'next/navigation';
 
 export default async function Page() {
@@ -11,8 +11,8 @@ export default async function Page() {
   logout();
 
   // 2. 쿠키에서 액세스 토큰 삭제
-  const cookieStore = cookies();
-  (await cookieStore).delete('accessToken');
+  // const cookieStore = cookies();
+  // (await cookieStore).delete('accessToken');
 
   // 3. 완료되면 /login으로 이동
   redirect('/login');
