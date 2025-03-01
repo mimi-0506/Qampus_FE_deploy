95151b4b2e802befac6e517818b161d78dc33367
mimi <0506phm@gmail.com>
2025-03-02 02:37:53 +0900

    style: 커뮤니티 css 수정

--- app/community/_component/Info.tsx
+++ app/community/_component/Info.tsx
@@ -67,6 +67,12 @@ export default function Info() {
       </div>
 
       <Slider top={'top-[45vw]'} data={data} />
+
+      <div className="relative overflow-hidden w-[111vw]  h-[10vw] left-[-5vw] top-[-9vw] flex justify-center">
+        <div className="absolute left-0 top-0 w-[111vw] aspect-[2132/585] z-10">
+          <Image fill src="/images/community/bottom.png" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
