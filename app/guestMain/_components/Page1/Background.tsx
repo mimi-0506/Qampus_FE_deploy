05235eb75214b778a05dc929e9c71a284d9c01aa
mimi <0506phm@gmail.com>
2025-03-05 19:19:31 +0900

    style: page1 스타일 재정의

--- app/guestMain/_components/Page1/Background.tsx
+++ app/guestMain/_components/Page1/Background.tsx
@@ -2,31 +2,30 @@ import Image from 'next/image';
 
 export default function Background() {
   return (
-    <div className="absolute top-0 z-0 h-screen w-full">
-      <div className="w-[110vw] aspect-[2105/400] mt-[11.15vw] ml-[9.52vw]">
-        <Image
-          src="/images/main/bg_logo.png"
-          alt="bg text"
-          width={2105}
-          height={380}
-        />
-      </div>
+    <div className="absolute top-0 min-h-screen bg-white h-screen w-full">
+      <Image
+        src="/images/logo/logo_big.png"
+        alt="bg text"
+        width={2105}
+        height={380}
+        sizes={'110vw'}
+        className="mt-[5vw] ml-[9.52vw]"
+      />
 
-      <div className="w-[110vw] aspect-[2105/400] relative top-[2.60vw] right-[15.63vw]">
-        <Image
-          src="/images/main/bg_logo.png"
-          alt="bg text"
-          width={2105}
-          height={380}
-        />
-      </div>
+      <Image
+        src="/images/logo/logo_bigpng"
+        alt="bg text"
+        width={2105}
+        height={380}
+        sizes={'110vw'}
+        className=" relative top-[2.60vw] right-[15.63vw]"
+      />
 
       <Image
         src="/images/main/gradient_page1.png"
         alt=""
         width={1700}
         height={751}
-        layout="responsive"
         className="absolute top-[30.15vw]"
       />
     </div>
