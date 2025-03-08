57ea963f9312881620dc1afad54e28ac9cb3e027
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-03-08 20:58:03 +0900

    feat: 라우팅 페이지에 따른 로그인 컴포넌트 색 변경
    * hydration 방지를 위헤 useEffect로 isCommunity 값을 클라이언트에서만 설정하도록 수정
    * login / community 페이지에서만 로그인 컴포넌트 색상 변경

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -13,10 +13,12 @@ const NAV_ITEMS = [
 
 const Header = () => {
   const pathname = usePathname();
-  const isCommunity = pathname === '/community' || '/login';
+  const [isCommunity, setIsCommunity] = useState(false);
   const [accessToken, setAccessToken] = useState<string | null>(null);
 
   useEffect(() => {
+    setIsCommunity(pathname === '/community' || pathname === '/login');
+
     // accessToken 확인
     const cookies = document.cookie
       .split('; ')
@@ -25,11 +27,13 @@ const Header = () => {
     if (cookies) {
       setAccessToken(cookies.split('=')[1]);
     }
-  }, []);
+  }, [pathname]);
 
   return (
     <header
-      className={`fixed top-0 left-0 w-full h-[80px] z-50 flex items-center ${isCommunity ? 'bg-black' : 'bg-white'}`}
+      className={`fixed top-0 left-0 w-full h-[80px] z-50 flex items-center ${
+        isCommunity ? 'bg-black' : 'bg-white'
+      }`}
     >
       <div className="mx-auto flex w-[95%] items-center justify-between px-6">
         {/* 로고 아이콘 */}
@@ -49,7 +53,9 @@ const Header = () => {
           {NAV_ITEMS.map(({label, path}) => (
             <Link key={path} href={path}>
               <span
-                className={`underline-offset-8 ${isCommunity ? 'text-white' : 'text-black'} ${
+                className={`underline-offset-8 ${
+                  isCommunity ? 'text-white' : 'text-black'
+                } ${
                   pathname === path
                     ? 'font-semibold underline'
                     : 'hover:underline'
@@ -61,6 +67,7 @@ const Header = () => {
           ))}
         </nav>
 
+        {/* 로그인/마이페이지 */}
         <div className="ml-16">
           {accessToken ? (
             // 로그인한 경우 (마이페이지 아이콘 표시)
@@ -78,14 +85,24 @@ const Header = () => {
             // 로그인하지 않은 경우 (로그인 버튼 표시)
             <Link
               href="/login"
-              className="flex items-center px-3 py-2 gap-2 rounded-full shadow-md bg-white shadow-[0px_0px_2px_0px_rgba(0,0,0,0.15)]"
+              className={`flex items-center px-3 py-2 gap-2 rounded-full shadow-md ${
+                isCommunity ? 'bg-black text-white' : 'bg-white text-black'
+              } shadow-[0px_0px_2px_0px_rgba(0,0,0,0.15)]`}
             >
-              <span className="text-black text-xs font-300">로그인</span>
+              <span
+                className={`text-xs font-300 ${isCommunity ? 'text-white' : 'text-black'}`}
+              >
+                로그인
+              </span>
               <Image
-                src="/svg/right-arrow-black.svg"
+                src={
+                  isCommunity
+                    ? '/svg/right-arrow-white.svg'
+                    : '/svg/right-arrow-black.svg'
+                }
                 alt="arrow"
-                width={8}
-                height={8}
+                width={7}
+                height={7}
               />
             </Link>
           )}
