e553c67f57b1dca46ff8a05186ba223e66eda0df
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-03-08 18:45:21 +0900

    fix: 타입 에러 수정

--- components/ViewAnswer.tsx
+++ components/ViewAnswer.tsx
@@ -92,7 +92,9 @@ export default function ViewAnswer({
             onSelect={handleSelectAnswer}
             chooseAnswerId={chooseAnswerId}
             isCentered={centeredAnswerIds.includes(nowAnswer.answerId)}
-            ref={el => (answerRefs.current[index] = el)}
+            ref={el => {
+              answerRefs.current[index] = el;
+            }}
           />
         ))}
     </>
