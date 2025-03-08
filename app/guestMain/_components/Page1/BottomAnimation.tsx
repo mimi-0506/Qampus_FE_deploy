c7f5bc158a11e265b1461d614e0964e1aad16464
mimi <0506phm@gmail.com>
2025-03-08 20:44:22 +0900

    style: bottom 애니메이션 이미지 수정

--- app/guestMain/_components/Page1/BottomAnimation.tsx
+++ app/guestMain/_components/Page1/BottomAnimation.tsx
@@ -10,7 +10,7 @@ export default function BottomAnimation() {
   return (
     <div className="text-dark2 bg-page1anibg flex shrink-0 animate-loop items-center justify-start gap-[3vw] pr-[5vw]">
       <div className="w-[8vw] aspect-[155/30] relative">
-        <Image src="/images/logo/logo_mini.png" alt="logo" fill />
+        <Image src="/images/logo/logo_mini_dark.png" alt="logo" fill />
       </div>
 
       <div className="w-[6vw] h-[1vw] bottom-[0.4vw] relative flex justify-center items-center">
