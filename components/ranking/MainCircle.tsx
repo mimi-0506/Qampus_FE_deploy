862cc0978f0168625bd20752d27ab1b4b5f1c190
mimi <0506phm@gmail.com>
2025-03-12 15:26:44 +0900

    hotfix: overflow-hidden추가

--- components/ranking/MainCircle.tsx
+++ components/ranking/MainCircle.tsx
@@ -4,7 +4,7 @@ export default function MainCircle({image}: {image: number | undefined}) {
   return (
     <div className="w-[13vw] aspect-[1/1] rounded-full bg-gradient-to-b from-white to-blue-600 flex items-center justify-center shadow-2xl border-[0.6vw] border-blue-800">
       <div className="w-[83%] aspect-[1/1] bg-page5roundbg border border-white rounded-full flex items-center justify-center">
-        <div className="w-[82%] aspect-[1/1] relative rounded-full bg-white flex items-center justify-center">
+        <div className="w-[82%] aspect-[1/1] overflow-hidden relative rounded-full bg-white flex items-center justify-center">
           <Image src={`/images/univ/${image}.png`} alt="univ logo" fill />
         </div>
       </div>
