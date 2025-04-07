2d78a94dc88bd656737e1818e8be942838fb1f58
mimi <62527898+mimi-0506@users.noreply.github.com>
2025-04-07 19:21:58 +0900

    fix: 캐러셀 애니메이션 이미지 중앙정렬 추가

--- components/ranking/Slider/MainCircle.tsx
+++ components/ranking/Slider/MainCircle.tsx
@@ -4,7 +4,7 @@ export default function MainCircle({id}: {id: number}) {
   return (
     <div className="w-full aspect-[1/1] rounded-full overflow-hidden bg-gradient-to-b from-white to-blue-600 flex items-center justify-center border-[0.6vw] border-blue-800">
       <div className="w-[83%] h-auto overflow-hidden aspect-[1/1] bg-page5roundbg border border-white rounded-full flex items-center justify-center">
-        <div className="w-[82%] h-auto aspect-[1/1] relative bg-white rounded-full overflow-hidden">
+        <div className="w-[82%] h-auto aspect-[1/1] relative bg-white rounded-full overflow-hidden  flex items-center justify-center">
           <Image
             src={`/images/univ/${id}.png`}
             alt="univ logo"
